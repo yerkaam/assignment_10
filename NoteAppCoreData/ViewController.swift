7bb2f82c821ca6171fc7a1a0f4d44c659f12e87e
yerkaam <eraorynbai@gmail.com>
2024-12-08 14:34:00 +0500

    Initial Commit

---
+++ NoteAppCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteAppCoreData
+//
+//  Created by Yerdaulet Orynbay on 08.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
